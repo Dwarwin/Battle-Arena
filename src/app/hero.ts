ec97383c66b0e589cd684f3fb23ed386d469ccde
Dmitriy <dwarwin@gmail.com>
2018-12-03 17:29:41 +0200

    workflow +

--- src/app/hero.ts
+++ src/app/hero.ts
@@ -20,8 +20,8 @@ export class Hero {
     this.luck = luck || 10;
     this.freeStats = 50 - this.strength - this.dexterity - this.constitution - this.luck;
     this.heroHP = this.constitution * 10;
-    this.evadeChance = Math.round(10 + (this.dexterity - 10) * 7);
-    this.criticalHitChance = Math.round(10 + (this.luck - 9) * 10);
+    this.evadeChance = Math.round((10 + this.dexterity - 10) * 7);
+    this.criticalHitChance = Math.round((10 + this.luck - 10) * 9);
     this.heroDamage = 10 + Math.round((this.strength - 10));
   }
 }
