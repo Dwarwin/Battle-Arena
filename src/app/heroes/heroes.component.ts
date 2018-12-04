7e872b59239d02246369ee043a8373c115ef0152
Dmitriy <dwarwin@gmail.com>
2018-12-05 00:31:27 +0200

    workflow +details

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -15,6 +15,8 @@ export class HeroesComponent implements OnInit {
   heroes: Hero[];
   hero: Hero;
   name: FormControl;
+  heroAdded: boolean;
+  showDetail: number;
 
   constructor(private heroService: HeroService,
               private router: Router) {
@@ -35,8 +37,10 @@ export class HeroesComponent implements OnInit {
   }
 
   add(name: string): void {
+    this.heroAdded = true;
     name = name.trim();
     if (!name || this.heroes.find((_) => _.name.toLowerCase() === name.toLowerCase())) {
+      this.heroAdded = false;
       return;
     }
     this.heroService.addHero({name} as Hero)
@@ -56,6 +60,10 @@ export class HeroesComponent implements OnInit {
   delete(hero: Hero): void {
     this.heroes = this.heroes.filter(h => h !== hero);
     this.heroService.deleteHero(hero).subscribe();
+  }
+
+  showDetails(hero): void {
+    this.showDetail = hero;
   }
 }
 
