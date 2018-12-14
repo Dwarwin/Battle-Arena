6291a24941b3ae0bd6ef9da2c47aa68da4515716
Dmitriy <dwarwin@gmail.com>
2018-12-14 02:39:14 +0200

    workflow unsubscribe model

--- src/app/hero-model/hero-model.component.ts
+++ src/app/hero-model/hero-model.component.ts
@@ -1,9 +1,9 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnDestroy, OnInit} from '@angular/core';
 
-import { Hero } from '../hero';
-import { HeroParts, EnemyHeroParts, Parts } from '../heroParts';
-import { BattleService } from '../services/battle.service';
-import { Subscription } from 'rxjs';
+import {Hero} from '../hero';
+import {HeroParts, EnemyHeroParts, Parts} from '../heroParts';
+import {BattleService} from '../services/battle.service';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-hero-model',
@@ -11,34 +11,51 @@ import { Subscription } from 'rxjs';
   styleUrls: ['./hero-model.component.sass']
 })
 
-export class HeroModelComponent implements OnInit {
+export class HeroModelComponent implements OnInit, OnDestroy {
 
   @Input() enemy: boolean;
   @Input() battleEnded: boolean;
   hero: Hero;
   currentHP: number;
+  partsSelected: boolean;
   heroParts: Parts[] = HeroParts;
   enemyParts: Parts[] = EnemyHeroParts;
   heroParts$: Subscription;
   enemyParts$: Subscription;
-  partsSelected: boolean;
+  yourHeroHP$: Subscription;
+  enemyHeroHP$: Subscription;
 
-  constructor( private battleService: BattleService ) {}
+  constructor(private battleService: BattleService) {
+  }
 
   ngOnInit() {
     const heroSide = !this.enemy ? this.battleService.setYourHero$ : this.battleService.setEnemyHero$;
     heroSide.subscribe(val => this.hero = val);
-    !this.enemy ? this.heroParts$ = this.battleService.blockedPoints
-        .subscribe(el => el.length < 2 ? this.partsSelected = false : this.partsSelected = true)
-      : this.enemyParts$ = this.battleService.attackedPoints
-        .subscribe(el => el.length < 2 ? this.partsSelected = false : this.partsSelected = true);
+    this.getHeroesParts();
     this.getCurrentHP();
   }
 
+  ngOnDestroy(): void {
+    if (!this.enemy) {
+      this.heroParts$.unsubscribe();
+      this.yourHeroHP$.unsubscribe();
+    } else {
+      this.enemyParts$.unsubscribe();
+      this.enemyHeroHP$.unsubscribe();
+    }
+  }
+
+  getHeroesParts(): void {
+    !this.enemy ? this.heroParts$ = this.battleService.blockedPoints$
+        .subscribe(el => el.length < 2 ? this.partsSelected = false : this.partsSelected = true)
+      : this.enemyParts$ = this.battleService.attackedPoints$
+        .subscribe(el => el.length < 2 ? this.partsSelected = false : this.partsSelected = true);
+  }
+
   getCurrentHP(): void {
-    this.enemy
-      ? this.battleService.enemyHeroHP$.subscribe(hp => this.currentHP = hp)
-      : this.battleService.yourHeroHP$.subscribe(hp => this.currentHP = hp);
+    !this.enemy
+      ? this.yourHeroHP$ = this.battleService.yourHeroHP$.subscribe(hp => this.currentHP = hp)
+      : this.enemyHeroHP$ = this.battleService.enemyHeroHP$.subscribe(hp => this.currentHP = hp);
   }
 
   choosePoint(point: Parts): void {
